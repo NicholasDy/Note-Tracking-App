fbbf1888d3f6e19788858d49982687e24ff819ce
Nick <nicholasdy272@gmail.com>
2021-04-14 14:59:44 -0400

    updating port

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 require('./Routes/htmlRoute')(app)
 require('./Routes/apiRoute')(app)
