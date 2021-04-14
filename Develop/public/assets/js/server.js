133fa598542e36dede8df15f5c139c2f2ee4686d
Nick <nicholasdy272@gmail.com>
2021-04-14 14:51:52 -0400

    heroku tets

---
+++ Develop/public/assets/js/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const PORT = 3001
+
+require('./Routes/htmlRoute')(app)
+require('./Routes/apiRoute')(app)
+
+app.listen(PORT, () => console.log(`App listening on Port ${PORT}`));
+
+
+// have to link the two htmls correctly for the page to start working properly
